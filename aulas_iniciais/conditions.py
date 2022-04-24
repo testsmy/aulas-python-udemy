abe312c802bc8449c67a0df114c1bbd756b87607
testsmy <luzivangois33@outlook.com>
2022-04-24 20:04:04 -0300

    update conditions

--- aulas_iniciais/conditions.py
+++ aulas_iniciais/conditions.py
@@ -3,9 +3,10 @@
 name = input('Digite o seu nome: ')
 age = int(input('Digite a sua idade: '))
 print('Estamos analisando se você pode dirigir...')
-if (age < 18):
-    print('Vixe! Você já pode dirigir boy!')
-elif (age >= 18 and age <= 60)
+
+if age < 18:
+    print('Vixe! Você não pode dirigir boy!')
+elif age >= 18 and age <= 60:
     print('Parabéns, você pode dirigir sem restrições')
 else:
     print('Legal, você pode dirigir, mas com restrições!')
