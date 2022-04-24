ba4b713f3e4d5c60e74738c6536eec470b9ef366
testsmy <luzivangois33@outlook.com>
2022-04-24 20:53:52 -0300

    update others

---
+++ aulas_iniciais/othes_sintaxe.py
@@ -0,0 +1,48 @@
+# len -> count numbers characters in string
+
+""" text_count = input('Digite o texto a ser contado:')
+numbers_char = len(text_count)
+
+print(f'O texto {text_count} possui {numbers_char} caracteres!') """
+
+
+# functions ISDECIMAL, ISNUMERIC, ISDIGIT
+""" number1 = input('Digite o primeiro número: ')
+number2 = input('Digite o segundo número: ')
+
+# Valide number typed
+if number1.isdigit() and number2.isdigit():
+    number1 = int(number1)
+    number2 = int(number2)
+    print( number1 + number2)
+else:
+    print('Você não digitou um número, malandrinho!') """
+
+
+# try, except
+
+""" number1 = input('Digite o primeiro número: ')
+number2 = input('Digite o segundo número: ')
+
+try:
+    int(number1) > 5
+    int(number2) > 5
+    print( number1 + number2 )
+except:
+    print('Deu FALHA BROTHER!')
+ """
+
+
+# placeholdes -> pass, ...
+
+value = False
+
+""" if value:
+    ...
+else:
+    print('Pulouuu') """
+
+if value:
+    pass
+else:
+    print('Passou')
