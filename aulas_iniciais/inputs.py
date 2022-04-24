3a2583b932610ea6e2a76fbec21cab9934fde70e
testsmy <luzivangois33@outlook.com>
2022-04-24 19:09:20 -0300

    update

--- aulas_iniciais/inputs.py
+++ aulas_iniciais/inputs.py
@@ -1,3 +1,8 @@
 #Working of inputs
-name = input('Qual o seu nome:')
-print(name)
+# name = input('Qual o seu nome:')
+# print(name)
+
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+
+print( number1 + number2 )
