585523e71c4fabacab12ff84284378ab16ae86e8
testsmy <luzivangois33@outlook.com>
2022-04-24 17:11:50 -0300

    update

---
+++ AulasIniciais/operators.py
@@ -0,0 +1,8 @@
+# + -> some
+# - -> subtration
+# * -> multiplication
+# / -> divide
+# // -> integer result
+# ** -> pontentiation
+# % -> rest result
+# ( ) -> precedences
---
+++ AulasIniciais/typesData.py
@@ -0,0 +1,5 @@
+#data type
+print(type('Luccas'))
+print(type(1510))
+print(type(55.8))
+print(type(True))
