14435b9e78bccd42047e2e46bf5a9ecdd458b8ba
testsmy <luzivangois33@outlook.com>
2022-04-30 20:02:39 -0300

    aula 31

---
+++ aulas_iniciais/format_values.py
@@ -0,0 +1,7 @@
+# Format values modifcators
+
+:s - strings
+:d - numbers intereger
+:f - floats numbers
+:.2 - quantication floats characters (exempl: 2)
+:(CHARACTER) (> OR < OR ^)(QUANTICATION)(TYPE - s, d OR f)
