0c24a9731850ebb41a8103d7a30063abd9efdd61
testsmy <luzivangois33@outlook.com>
2022-05-01 17:43:25 -0300

    update

--- aulas_iniciais/format_values.py
+++ aulas_iniciais/format_values.py
@@ -1,7 +1,19 @@
 # Format values modifcators
-
+""" 
 :s - strings
 :d - numbers intereger
 :f - floats numbers
 :.2 - quantication floats characters (exempl: 2)
 :(CHARACTER) (> OR < OR ^)(QUANTICATION)(TYPE - s, d OR f)
+
+> - LEFT
+< - RIGTH
+^ - CENTER
+ """
+
+number1 = 123456
+number2 = 2
+division = number1 / number2
+print('{:.2f}'.format(division))
+print(f'{division:.2f}')
+print(f'{number1:2>5}')
