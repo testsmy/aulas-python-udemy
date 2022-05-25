bca2de07583e256e5ea152c31ab079ddc3393fc5
testsmy <luzivangois33@outlook.com>
2022-05-24 21:36:16 -0300

    parabens israel

---
+++ aulas_iniciais/parabens_israel.py
@@ -0,0 +1,20 @@
+# programa que dá parabéns
+
+def listAlphabet():
+  return list(map(chr, range(97, 123)))
+
+print(listAlphabet()[15])
+print(listAlphabet()[0])
+print(listAlphabet()[17])
+print(listAlphabet()[0])
+print(listAlphabet()[1])
+print(listAlphabet()[4])
+print(listAlphabet()[13])
+print(listAlphabet()[18])
+print('')
+print(listAlphabet()[8])
+print(listAlphabet()[18])
+print(listAlphabet()[17])
+print(listAlphabet()[0])
+print(listAlphabet()[4])
+print(listAlphabet()[11])
